50f13c53c75e952d84488eb1820f2fed7c7e3a7c
BookLove <krouvy@mail.ru>
2021-12-23 16:01:20 +0300

    Проба 3

--- static/js/mesto.js
+++ static/js/mesto.js
@@ -3,6 +3,11 @@ var mar_left;
 var bground;
 var place_img;
 
-
+$( "#id_uf5220" ).click(function() {
+    mar_top = -106;
+    mar_left = 780;
+    bground = 'url(../images/apec.png)';
+    place_img = "../images/ii_ploshadka.png";
+});
 
 
