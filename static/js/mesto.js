9b1e12c40c73fc41e5f6137a2a79a9191212c143
BookLove <krouvy@mail.ru>
2021-12-23 16:40:54 +0300

    Попытка 6, + доп логи.

--- static/js/mesto.js
+++ static/js/mesto.js
@@ -17,7 +17,21 @@ function uf5220 (){
     mar_left = 780;
     bground = 'url(../images/apec.png)';
     place_img = "../images/ii_ploshadka.png";
-    console.log(mar_top,mar_left, bground, place_img)
+    console.log(mar_top,mar_left, bground, place_img);
+    ready ();
 };
 
 
+function ready () {
+    $(document).ready(function(){
+        $(".icon").css('marginTop', mar_top);
+        $(".icon").css('marginLeft', mar_left);
+        $('.icon').css('backgroundImage', bground)
+        $(".place_img").attr("src", place_img);
+        $(".pulse2").css('marginTop', mar_top - 10);
+        $(".pulse2").css('marginLeft', mar_left - 10);
+        alert("Внутри")
+    });
+    alert("Снарутжи")
+    console.log(mar_top,mar_left, bground, place_img);
+};
