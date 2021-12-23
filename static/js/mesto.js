f57df7013e179bcabb78076d09328ced813742ec
BookLove <krouvy@mail.ru>
2021-12-23 16:19:57 +0300

    Проба 5

--- static/js/mesto.js
+++ static/js/mesto.js
@@ -4,10 +4,19 @@ var bground;
 var place_img;
 
 $( "#id_uf5220" ).click(function() {
+    alert("Функция отработала")
     mar_top = -106;
     mar_left = 780;
     bground = 'url(../images/apec.png)';
     place_img = "../images/ii_ploshadka.png";
 });
+
+function uf5220 (){
+    alert("Функция 2 отработала")
+    mar_top = -106;
+    mar_left = 780;
+    bground = 'url(../images/apec.png)';
+    place_img = "../images/ii_ploshadka.png";
+};
 
 
