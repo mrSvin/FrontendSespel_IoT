e2800111d201e1df5ff89fe79b5bd84e84a176e3
BookLove <krouvy@mail.ru>
2021-12-24 15:47:20 +0300

    Исправление ошибки

---
+++ static/js/mesto.js
@@ -0,0 +1,29 @@
+// начало мест второй площадки
+function uf5220 (){
+    mar_top = -115;
+    mar_left = 875;
+    bground = 'url(../images/UVF_5220.png)';
+    place_img = "../images/ii_ploshadka.png";
+    // Изменения значений
+
+    window.localStorage['mar_top'] = mar_top;
+    window.localStorage['mar_left'] = mar_left;
+    window.localStorage['bground'] = bground;
+    window.localStorage['place_img'] = place_img;
+    // Сохранение значений
+};
+
+function progress (){
+    mar_top = -105;
+    mar_left = 494;
+    bground = 'url(../images/progress.png)';
+    place_img = "../images/ii_ploshadka.png";
+
+    window.localStorage['mar_top'] = mar_top;
+    window.localStorage['mar_left'] = mar_left;
+    window.localStorage['bground'] = bground;
+    window.localStorage['place_img'] = place_img;
+};
+
+// конец мест второй площадки
+
