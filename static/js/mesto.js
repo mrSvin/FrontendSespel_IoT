31a529458199a9144c2dd60f8a192ba2374000fb
BookLove <krouvy@mail.ru>
2021-12-24 17:08:58 +0300

    Места для всех станков в суточных отчетах второй площадки

--- static/js/mesto.js
+++ static/js/mesto.js
@@ -2,27 +2,158 @@
 function uf5220 (){
     mar_top = -115;
     mar_left = 875;
-    bground = 'url(../images/UVF_5220.png)';
+    bground = 'url(../images/UVF_5220.png) no-repeat';
     place_img = "../images/ii_ploshadka.png";
+    size = 40;
     // Изменения значений
 
     window.localStorage['mar_top'] = mar_top;
     window.localStorage['mar_left'] = mar_left;
     window.localStorage['bground'] = bground;
     window.localStorage['place_img'] = place_img;
+    window.localStorage['size'] = size;
     // Сохранение значений
 };
 
 function progress (){
     mar_top = -105;
     mar_left = 494;
-    bground = 'url(../images/progress.png)';
+    bground = 'url(../images/progress.png) no-repeat';
     place_img = "../images/ii_ploshadka.png";
+    size = 40;
 
     window.localStorage['mar_top'] = mar_top;
     window.localStorage['mar_left'] = mar_left;
     window.localStorage['bground'] = bground;
     window.localStorage['place_img'] = place_img;
+    window.localStorage['size'] = size;
+};
+
+function ntx1000_1 (){
+    mar_top = -100;
+    mar_left = 295;
+    bground = 'url(../images/ntx1000.png) no-repeat';
+    place_img = "../images/ii_ploshadka.png";
+    size = 25;
+
+    window.localStorage['mar_top'] = mar_top;
+    window.localStorage['mar_left'] = mar_left;
+    window.localStorage['bground'] = bground;
+    window.localStorage['place_img'] = place_img;
+    window.localStorage['size'] = size;
+};
+
+function sk50 (){
+    mar_top = -100;
+    mar_left = 597;
+    bground = 'url(../images/sk50.png) no-repeat';
+    place_img = "../images/ii_ploshadka.png";
+    size = 40;
+
+    window.localStorage['mar_top'] = mar_top;
+    window.localStorage['mar_left'] = mar_left;
+    window.localStorage['bground'] = bground;
+    window.localStorage['place_img'] = place_img;
+    window.localStorage['size'] = size;
+};
+
+function apec (){
+    mar_top = -106;
+    mar_left = 780;
+    bground = 'url(../images/apec.png) no-repeat';
+    place_img = "../images/ii_ploshadka.png";
+    size = 40;
+
+    window.localStorage['mar_top'] = mar_top;
+    window.localStorage['mar_left'] = mar_left;
+    window.localStorage['bground'] = bground;
+    window.localStorage['place_img'] = place_img;
+    window.localStorage['size'] = size;
+};
+
+function dmg_dmu50_3 (){
+    mar_top = -65;
+    mar_left = 295;
+    bground = 'url(../images/dmg_dmu50.png) no-repeat';
+    place_img = "../images/ii_ploshadka.png";
+    size = 25;
+
+    window.localStorage['mar_top'] = mar_top;
+    window.localStorage['mar_left'] = mar_left;
+    window.localStorage['bground'] = bground;
+    window.localStorage['place_img'] = place_img;
+    window.localStorage['size'] = size;
+};
+
+function dmg_dmu50_4 (){
+    mar_top = -65;
+    mar_left = 270;
+    bground = 'url(../images/dmg_dmu50.png) no-repeat';
+    place_img = "../images/ii_ploshadka.png";
+    size = 25;
+
+    window.localStorage['mar_top'] = mar_top;
+    window.localStorage['mar_left'] = mar_left;
+    window.localStorage['bground'] = bground;
+    window.localStorage['place_img'] = place_img;
+    window.localStorage['size'] = size;
+};
+
+function dmg_ctx310_2 (){
+    mar_top = -65;
+    mar_left = 235;
+    bground = 'url(../images/dmg_ctx310.png) no-repeat';
+    place_img = "../images/ii_ploshadka.png";
+    size = 25;
+
+    window.localStorage['mar_top'] = mar_top;
+    window.localStorage['mar_left'] = mar_left;
+    window.localStorage['bground'] = bground;
+    window.localStorage['place_img'] = place_img;
+    window.localStorage['size'] = size;
+};
+
+function dmg_ctx510_2 (){
+    mar_top = -105;
+    mar_left = 235;
+    bground = 'url(../images/dmg_ctx510.png) no-repeat';
+    place_img = "../images/ii_ploshadka.png";
+    size = 25;
+
+    window.localStorage['mar_top'] = mar_top;
+    window.localStorage['mar_left'] = mar_left;
+    window.localStorage['bground'] = bground;
+    window.localStorage['place_img'] = place_img;
+    window.localStorage['size'] = size;
+};
+
+function dmg_ctx510_3 (){
+    mar_top = -65;
+    mar_left = 210;
+    bground = 'url(../images/dmg_ctx510.png) no-repeat';
+    place_img = "../images/ii_ploshadka.png";
+    size = 25;
+
+    window.localStorage['mar_top'] = mar_top;
+    window.localStorage['mar_left'] = mar_left;
+    window.localStorage['bground'] = bground;
+    window.localStorage['place_img'] = place_img;
+    window.localStorage['size'] = size;
+};
+
+
+function dmg_ctx310_3 (){
+    mar_top = -105;
+    mar_left = 210;
+    bground = 'url(../images/dmg_ctx310.png) no-repeat';
+    place_img = "../images/ii_ploshadka.png";
+    size = 25;
+
+    window.localStorage['mar_top'] = mar_top;
+    window.localStorage['mar_left'] = mar_left;
+    window.localStorage['bground'] = bground;
+    window.localStorage['place_img'] = place_img;
+    window.localStorage['size'] = size;
 };
 
 // конец мест второй площадки
