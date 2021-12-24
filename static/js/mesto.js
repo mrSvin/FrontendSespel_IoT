4609e1c4de3d9ad25e83b26313491da9606c58b0
BookLove <krouvy@mail.ru>
2021-12-24 14:58:28 +0300

    Передача через отдельный скрипт, реальные данные для прогресса и УФ

--- static/js/mesto.js
+++ static/js/mesto.js
@@ -1,18 +1,34 @@
-// $( "#id_uf5220" ).click(function() {
-//     alert("Функция отработала")
-//     mar_top = -106;
-//     mar_left = 780;
-//     bground = 'url(../images/apec.png)';
-//     place_img = "../images/ii_ploshadka.png";
-// });
+var mar_top = localStorage.getItem('mar_top');
+var mar_left = localStorage.getItem('mar_left');
+var bground = localStorage.getItem('bground');
+var place_img = localStorage.getItem('place_img');
 
 function uf5220 (){
-    alert("Функция 2 отработала")
-    mar_top = -106;
-    mar_left = 780;
-    bground = 'url(../images/apec.png)';
+    mar_top = -115;
+    mar_left = 875;
+    bground = 'url(../images/UVF_5220.png)';
     place_img = "../images/ii_ploshadka.png";
+    // Изменения значений
+
+    window.localStorage['mar_top'] = mar_top;
+    window.localStorage['mar_left'] = mar_left;
+    window.localStorage['bground'] = bground;
+    window.localStorage['place_img'] = place_img;
+    // Сохранение значений
+
+
+
 };
 
+function progress (){
+    mar_top = -105;
+    mar_left = 494;
+    bground = 'url(../images/progress.png)';
+    place_img = "../images/ii_ploshadka.png";
 
+    window.localStorage['mar_top'] = mar_top;
+    window.localStorage['mar_left'] = mar_left;
+    window.localStorage['bground'] = bground;
+    window.localStorage['place_img'] = place_img;
 
+};
