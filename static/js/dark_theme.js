4c96140e22e0747f9079f431c30b3d19e06662fb
BookLove <krouvy@mail.ru>
2022-02-24 17:32:18 +0300

    Пуш

--- static/js/dark_theme.js
+++ static/js/dark_theme.js
@@ -187,6 +187,7 @@ function hightcharts_colors(back_color, text_color, calen_1_color, calen_3_color
 function mesto_personal(calen_1_color, back_color){
     $('.label_mesto').attr('style','color:' + calen_1_color + ';'); // надпись место
     $('.label_personal').attr('style','color:' + calen_1_color + ';'); // надпись персонал
+    $('.label_error').attr('style','color:' + calen_1_color + ';'); // надпись персонал
 
     $(".icon_mesto").hover(function() { // свечение при наведении на место
         $(this).css('-webkit-box-shadow', 'inset 0px 0px 12px 12px' + back_color);
