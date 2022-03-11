20f75f94d0e540400e59aa7eb9e10e8b90e64e6a
BookLove <krouvy@mail.ru>
2022-03-11 15:54:23 +0300

    Фиксы и пробы

---
+++ static/js/month/universal.js
@@ -0,0 +1,104 @@
+function setData(containerLine,containerRound, arrayAvar, arrayFail, arrayPass, arrayNagruzka, arrayWork, avar, fail, pass, nagruzka, work) {
+    Highcharts.chart(containerLine, {
+        chart: {
+            type: 'column'
+        },
+        colors: colors,
+        title: {
+            text: 'Загрузка оборудования по дням'
+        },
+        xAxis: {
+            title: {
+                text: 'Дни месяца',
+                align: 'high'
+            },
+            labels: {
+                style: {
+                    fontSize: '18px',
+                }
+            },
+            categories: ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20', '21', '22', '23', '24', '25', '26', '27', '28', '29', '30', '31']
+        },
+        credits: {
+            enabled: false
+        },
+        yAxis: {
+            min: 0,
+            title: {
+                text: '%'
+            }
+        },
+        tooltip: {
+            pointFormat: '<span style="color:{series.color}">{series.name}</span>: {point.percentage:.1f}%<br/>',
+            shared: true
+        },
+        plotOptions: {
+            column: {
+                dataLabels: {
+                    enabled: false
+                }
+            }
+        },
+        legend: {
+            enabled: false
+        },
+
+        series: [{
+            name: 'Авария',
+            data: arrayAvar
+        }, {
+            name: 'Выключен',
+            data: arrayFail
+        }, {
+            name: 'Ожидание',
+            data: arrayPass
+        }, {
+            name: 'Под нагрузкой',
+            data: arrayNagruzka
+        }, {
+            name: 'Работа',
+            data: arrayWork
+        }]
+    });
+
+    Highcharts.chart(containerRound, {
+        chart: {
+            plotBackgroundColor: null,
+            plotBorderWidth: null,
+            plotShadow: false,
+            type: 'pie'
+        },
+
+        title: {
+            text: 'Загрузка оборудования'
+        },
+        tooltip: {
+            pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
+        },
+
+        accessibility: {
+            point: {
+                valueSuffix: '%'
+            }
+        },
+        plotOptions: {
+            pie: {
+                allowPointSelect: true,
+                cursor: 'pointer',
+                dataLabels: {
+                    enabled: false
+                },
+                showInLegend: true
+            }
+        },
+        colors:colors,
+        credits: {
+            enabled: false
+        },
+        series : [ {
+            type : 'pie',
+            name : 'Показатель',
+            data : [[ 'Работа', work ], [ 'Включен', pass ], [ 'Выключен', fail],  [ 'В аварии', avar ], [ 'Нагрузка', nagruzka ] ]
+        }]
+    });
+}
