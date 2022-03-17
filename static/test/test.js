f1bed55a5b646b229ecb2ae78b5a98f9810fc889
BookLove <krouvy@mail.ru>
2022-03-17 17:19:50 +0300

    Go

--- static/test/test.js
+++ static/test/test.js
@@ -1,5 +1,21 @@
-const complex_apec = {nameContainerLine:"container_days5", nameContainerRound:"container5",
-    arrayAvar:apec_avar_day, arrayFail:apec_off_day, arrayPass:apec_pause_day, arrayNagruzka: Array(31).fill(0),
-    arrayWork:apec_rabota_day, avar:apec_avar, fail:apec_fail, pass:apec_pass, nagruzka:0,
-    work:apec_work};
 
+    // Переменные с массивами линейных и круговых диаграмм с сервера
+    var stp13m_array_linear = [[${stp13m_month_linear}]]
+    var stp13m_array_round = [[${stp13m_month_round}]]
+
+    const linearDiagram = [stp13m_array_linear, navigator_2_golova_1_array_linear,
+    navigator_2_golova_2_array_linear, navigator_3_array_linear, trulaser_array_linear,
+    kometa_1_array_linear, kometa_2_array_linear, kometa_3_array_linear, ar55_array_linear];
+
+    const roundDiagram = [stp13m_array_round, navigator_2_golova_1_array_round, navigator_2_golova_2_array_round,
+    navigator_3_array_round, trulaser_array_round, kometa_1_array_round,
+    kometa_2_array_round, kometa_3_array_round, ar55_array_round];
+
+
+
+    $("button").click(function() { // свечение при наведении на место
+        console.log("aaaaa");
+    });
+    $("button").hover(function() { // свечение при наведении на место
+        console.log("aaaaa");
+    });
