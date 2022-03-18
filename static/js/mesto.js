bab4e4211181bc79d64de53c37fbcd6a1631f48f
BookLove <krouvy@mail.ru>
2022-03-18 14:23:22 +0300

    dmc1035_2

--- static/js/mesto.js
+++ static/js/mesto.js
@@ -210,6 +210,22 @@ function dmg_ctx510_5 (){
     window.localStorage['size'] = size;
 };
 
+function dmg_dmc1035_2 (){
+    mar_top = -103;
+    mar_left = 171;
+    bground = 'url(../images/dmg_ctx510.png) no-repeat';
+    place_img = "../images/ii_ploshadka.png";
+    size = 25;
+    bwidth = "unset";
+
+    window.localStorage['mar_top'] = mar_top;
+    window.localStorage['mar_left'] = mar_left;
+    window.localStorage['bground'] = bground;
+    window.localStorage['place_img'] = place_img;
+    window.localStorage['bwidth'] = bwidth;
+    window.localStorage['size'] = size;
+};
+
 // конец мест второй площадки
 
 
