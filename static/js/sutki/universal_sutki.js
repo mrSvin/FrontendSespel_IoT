3252ca1c81066e46f407dc2eff4c4d0b30d50c33
BookLove <krouvy@mail.ru>
2022-03-21 16:38:43 +0300

    undefined замена на 1, для пробы

--- static/js/sutki/universal_sutki.js
+++ static/js/sutki/universal_sutki.js
@@ -66,8 +66,8 @@ function pars(arrayParse, y, arrayName=null)
     var arraySave = [] // Массив, который будет заполняться
 
     if (arrayParse[index_pars] == undefined){
-        console.log("Aaaaaaaaaaaaaaaaaaaaaaaaa")
-        return false;
+        arraySave.push({x:1, x2:1, y:1})
+        // return false;
     }
 
     // Определение длины цикла. Длина парсящего массива делить на 2 - 2. 300 = 148
