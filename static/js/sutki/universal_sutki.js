935b3f2119cfd2e04a49a9bf1624b6f976db14eb
BookLove <krouvy@mail.ru>
2022-03-21 15:56:37 +0300

    Скорее всего путаница с индексами

--- static/js/sutki/universal_sutki.js
+++ static/js/sutki/universal_sutki.js
@@ -41,27 +41,27 @@ time_miss = time.slice(0, 10) + " " + "23:59:59";
 // Функция преобразования в дате сегодняшнего дня значений 23:59:59
 function timeReplace(dataArray) {
     // индекс ограничение, чтобы не обрабатывать 5-ый массив с именем программы
-    var index = 0;
+    var index_time = 0;
     // выполнять если текущие время не равно 23:59:59
     if (time != time_miss) {
         // пробег по массивам до массива с именем программы
         while (index < 5) {
-            $.each(dataArray[index], function (i) {
+            $.each(dataArray[index_time], function (i) {
                 // если в массиве время равно текущей дате 23:59:59
-                if (dataArray[index][i] == time_miss) {
+                if (dataArray[index_time][i] == time_miss) {
                     // то записать в него значение предыдущего индекса
-                    dataArray[index][i] = dataArray[index][i-1]
+                    dataArray[index_time][i] = dataArray[index_time][i-1]
                 }
             });
             // после оконачния цикла each перейти к следующему массиву
-            index += 1;
+            index_time += 1;
         }
     }
 }
 
 function pars(arrayParse, y, arrayName=null)
 {
-    var index = 0; // Индекс по одному из циклов
+    var index_pars = 0; // Индекс по одному из циклов
     var arraySave = [] // Массив, который будет заполняться
 
     // Определение длины цикла. Длина парсящего массива делить на 2 - 2. 300 = 148
@@ -69,18 +69,18 @@ function pars(arrayParse, y, arrayName=null)
 
     // Если имя программы не передано в функцию, то массив формируется без нее
     if (arrayName == null){
-        while(index <= lengh)
+        while(index_pars <= lengh)
         {   // Парсинг
-            arraySave.push({x:(new Date(arrayParse[index*2])).getTime(), x2:(new Date(arrayParse[index * 2 + 1])).getTime(), y:y})
-            index += 1;
+            arraySave.push({x:(new Date(arrayParse[index_pars*2])).getTime(), x2:(new Date(arrayParse[index_pars * 2 + 1])).getTime(), y:y})
+            index_pars += 1;
         }
     }
     // Иначе в массив парсится переданный массив с именем программы
     else {
-        while(index <= lengh)
+        while(index_pars <= lengh)
         {   // Парсинг
-            arraySave.push({x:(new Date(arrayParse[index*2])).getTime(), x2:(new Date(arrayParse[index * 2 + 1])).getTime(), y:y, programname:arrayName[index]})
-            index += 1;
+            arraySave.push({x:(new Date(arrayParse[index_pars*2])).getTime(), x2:(new Date(arrayParse[index_pars * 2 + 1])).getTime(), y:y, programname:arrayName[index_pars]})
+            index_pars += 1;
         }
     }
     // Функция возвращает массив коллекциями, содержащими 2 или 3 объекта.
