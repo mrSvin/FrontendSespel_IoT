df8d321642a46cbade3a60861cd9662474d3e19e
BookLove <krouvy@mail.ru>
2022-03-21 10:08:24 +0300

    Тест моей новой механики для времени. Позже нужно будет проверить производительность.

--- static/js/sutki/universal_sutki.js
+++ static/js/sutki/universal_sutki.js
@@ -25,3 +25,30 @@ function pars(arrayParse, lengh, y, arrayName=null)
     // Функция возвращает массив коллекциями, содержащими 2 или 3 объекта.
     return arraySave
 }
+
+// Получение текущего времяни в формате toISO
+var time = new Date(new Date().toString().split('GMT')[0] + ' UTC').toISOString();
+// Преобразоавние времение в формат '2022-03-21 10:00:35'
+time = time.slice(0, 10) + " " + time.slice(11, 19);
+// Время, которое будет переписано в массивах 'текущая дата 23:59:59'
+time_miss = time.slice(0, 10) + " " + "23:59:59";
+
+function timeReplace(dataArray) {
+    // индекс ограничение, чтобы не обрабатывать 5-ый массив с именем программы
+    var index = 0;
+    // выполнять если текущие время не равно 23:59:59
+    if (time != time_miss) {
+        // пробег по массивам до массива с именем программы
+        while (index < 5) {
+            $.each(dataArray[index], function (i) {
+                // если в массиве время равно 23:59:59
+                if (dataArray[index][i-1] == time_miss) {
+                    // то записать в него значение предыдущего индекса
+                    dataArray[index][i-1] = time
+                }
+            });
+            // после оконачния цикла each перейти к следующему массиву
+            index += 1;
+        }
+    }
+}
