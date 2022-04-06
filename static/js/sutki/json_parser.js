7a0a47f83f99943a996e5b18b441aba1112bd045
BookLove <krouvy@mail.ru>
2022-04-06 10:10:05 +0300

    Проверка на деле

--- static/js/sutki/json_parser.js
+++ static/js/sutki/json_parser.js
@@ -13,7 +13,7 @@ var time_date = time.slice(0, 10)
 var time_start = time_date + " " + "00:00:00";
 var time_end = time_date + " " + "23:59:59";
 
-var string = ['dmg_dmu50_3']//, 'dmg_dmu50_2', 'dmg_dmu50_3'//, 'dmg_dmu50_4', 'dmg_dmu50_5', , 'dmg_dmu50_6', 'dmg_dmu50_7', , 'dmg_dmu50_8']
+var string = ['dmg_dmu50_1', 'dmg_dmu50_2', 'dmg_dmu50_3', 'dmg_dmu50_4', 'dmg_dmu50_5', 'dmg_dmu50_6', 'dmg_dmu50_7', 'dmg_dmu50_8']
 
 $.map(string, function (stanok)
     {
@@ -37,40 +37,43 @@ $.map(string, function (stanok)
 
 // Запрос к работе
         showGetResult(stanok, time_start, time_end, 1, function(data){
-                console.log("Вызвалась функция для работы")
-                $.map(data.time, function (element){
-                    time_rabota.push(element)
-                })
+                data.onload = () => {
+                    console.log("Вызвалась функция для работы")
+                    $.map(data.time, function (element){
+                        time_rabota.push(element)
+                    })
 
-                $.map(data.programName, function (element){
-                    programName_rabota.push(element)
-                })
+                    $.map(data.programName, function (element){
+                        programName_rabota.push(element)
+                    })
 
-                $.map(data.status, function (element){
-                    status_rabota.push(element)
-                })
+                    $.map(data.status, function (element){
+                        status_rabota.push(element)
+                    })
 
-                console.log(stanok, "Rabota_time ", time_rabota)
-                console.log(stanok, "programName ", programName_rabota)
-                console.log(stanok, "status_rabota", status_rabota)
-
+                    console.log(stanok, "Rabota_time ", time_rabota)
+                    console.log(stanok, "programName ", programName_rabota)
+                    console.log(stanok, "status_rabota", status_rabota)
+                }
             }
         );
 
 // Запрос к паузе
         showGetResult(stanok, time_start, time_end, 2, function(data){
 
-                console.log("Вызвалась функция для паузы")
-                $.map(data.time, function (element){
-                    time_pause.push(element)
-                })
+                data.onload = () => {
+                    console.log("Вызвалась функция для паузы")
+                    $.map(data.time, function (element) {
+                        time_pause.push(element)
+                    })
 
-                $.map(data.status, function (element){
-                    status_pause.push(element)
-                })
+                    $.map(data.status, function (element) {
+                        status_pause.push(element)
+                    })
 
-                console.log(stanok, "time_pause", time_pause)
-                console.log(stanok, "status_pause", status_pause)
+                    console.log(stanok, "time_pause", time_pause)
+                    console.log(stanok, "status_pause", status_pause)
+                }
 
             }
         );
@@ -78,17 +81,19 @@ $.map(string, function (stanok)
 // Запрос к выключенному
         showGetResult(stanok, time_start, time_end, 3, function(data){
 
-                console.log("Вызвалась функция для выключенного")
-                $.map(data.time, function (element){
-                    time_fail.push(element)
-                })
+                data.onload = () => {
+                    console.log("Вызвалась функция для выключенного")
+                    $.map(data.time, function (element) {
+                        time_fail.push(element)
+                    })
 
-                $.map(data.status, function (element){
-                    status_fail.push(element)
-                })
+                    $.map(data.status, function (element) {
+                        status_fail.push(element)
+                    })
 
-                console.log(stanok, "time_fail", time_fail)
-                console.log(stanok, "status_fail", status_fail)
+                    console.log(stanok, "time_fail", time_fail)
+                    console.log(stanok, "status_fail", status_fail)
+                }
 
             }
         );
@@ -97,33 +102,38 @@ $.map(string, function (stanok)
         showGetResult(stanok, time_start, time_end, 4, function(data){
                 console.log("Вызвалась функция для аварии")
 
-                $.map(data.time, function (element){
-                    time_avar.push(element)
-                })
+                data.onload = () => {
+                    $.map(data.time, function (element) {
+                        time_avar.push(element)
+                    })
 
-                $.map(data.status, function (element){
-                    status_avar.push(element)
-                })
+                    $.map(data.status, function (element) {
+                        status_avar.push(element)
+                    })
 
-                console.log(stanok, "time_avar", time_avar)
-                console.log(stanok, "status_avar", status_avar)
+                    console.log(stanok, "time_avar", time_avar)
+                    console.log(stanok, "status_avar", status_avar)
+                }
 
             }
         );
 
 // Запрос к нагрузки
         showGetResult(stanok, time_start, time_end, 5, function(data){
+
+            data.onload = () => {
                 console.log("Вызвалась функция для нагрузки")
-                $.map(data.time, function (element){
+                $.map(data.time, function (element) {
                     time_nagruzka.push(element)
                 })
 
-                $.map(data.status, function (element){
+                $.map(data.status, function (element) {
                     status_nagruzka.push(element)
                 })
 
                 console.log(stanok, "time_nagruzka", time_nagruzka)
                 console.log(stanok, "status_nagruzka", status_nagruzka)
+            }
 
             }
         );
@@ -168,6 +178,111 @@ console.log(1, status, time, program)
 
 var arrayStatus = appa.responseJSON.status
 var arrayTime = appa.responseJSON.time
+
+
+var time = new Date(new Date().toString().split('GMT')[0] + ' UTC').toISOString();
+
+// Преобразоавние времение в формат '2022-03-21 10:00:35'
+var time_date = time.slice(0, 10)
+var time_start = time_date + " " + "00:00:00";
+var time_end = time_date + " " + "23:59:59";
+
+var dmg_dmu50_1 = [[],[],[],[],[]]
+
+var string = ['dmg_dmu50_1', 'dmg_dmu50_2', 'dmg_dmu50_3', 'dmg_dmu50_4', 'dmg_dmu50_5', 'dmg_dmu50_6', 'dmg_dmu50_7','dmg_dmu50_8']
+// var string = ['navigator_1', 'navigator_2_golova_1', 'navigator_2_golova_2', 'navigator_3', ]
+
+$.map(string, function (stanok)
+{
+    // Цикл для отправки запроса тегам от 1 до 5
+    for(let i = 1; 5>i; i++)
+    {
+        // Эта функция должна быть объявлена внутри тела цикла, она вызывается из запросов
+        function showMessage(stanok, tag) {
+            console.log("________Вызвалась функция Message____________")
+            console.log("Станок", stanok, "Tag", tag)
+            console.log(programNameV);
+            console.log(statusV);
+            console.log(timeV);
+            console.log("\n\n\n")
+        }
+
+        console.log("start")
+        let adress = 'http://192.168.3.41:8080/api/complex/' + stanok + '_beginTime:' + time_start + '_endTime:' + time_end + '_statusTag:' + i
+
+        var programNameV
+        var statusV
+        var timeV
+
+        $.get( adress,
+            function( data ) {
+                console.log("________Внутри_запроса____________")
+                programNameV = data.programName;
+                statusV = data.status;
+                timeV = data.time;
+                console.log(adress, '\n', programNameV, statusV, timeV)
+                showMessage(stanok, i);
+            });
+    }
+
+})
+
+
+
+var time = new Date(new Date().toString().split('GMT')[0] + ' UTC').toISOString();
+
+// Преобразоавние времение в формат '2022-03-21 10:00:35'
+var time_date = time.slice(0, 10)
+var time_start = time_date + " " + "00:00:00";
+var time_end = time_date + " " + "23:59:59";
+
+var dmg_dmu50_1 = [[],[],[],[],[]]
+
+var string = ['dmg_dmu50_2']//, 'dmg_dmu50_2', 'dmg_dmu50_3', 'dmg_dmu50_4', 'dmg_dmu50_5', 'dmg_dmu50_6', 'dmg_dmu50_7','dmg_dmu50_8']
+// var string = ['navigator_1', 'navigator_2_golova_1', 'navigator_2_golova_2', 'navigator_3', ]
+
+$.map(string, function (stanok)
+{
+    // Цикл для отправки запроса тегам от 1 до 5
+    for(let i = 1; 5>i; i++)
+    {
+        // Эта функция должна быть объявлена внутри тела цикла, она вызывается из запросов
+
+        console.log("start")
+        let adress = 'http://192.168.3.41:8080/api/complex/' + stanok + '_beginTime:' + time_start + '_endTime:' + time_end + '_statusTag:' + i
+
+        var programNameV
+        var statusV
+        var timeV
+
+        $.get( adress,
+            function( data ) {
+                console.log("________Внутри_запроса____________")
+                programNameV = data.programName;
+                statusV = data.status;
+                timeV = data.time;
+                console.log(adress, '\n', programNameV, statusV, timeV)
+                showMessage(stanok, i);
+            });
+
+        function showMessage(stanok, tag) {
+            console.log("________Вызвалась функция Message____________")
+            console.log("Станок", stanok, "Tag", tag)
+            console.log(programNameV);
+            console.log(statusV);
+            console.log(timeV);
+            console.log("\n\n\n")
+        }
+
+    }
+
+})
+
+
+
+
+
+
 
 
 
