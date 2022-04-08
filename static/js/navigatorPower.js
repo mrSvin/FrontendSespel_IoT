3a856adee4050eb0618c9a00ee2d2e74689c4601
BookLove <krouvy@mail.ru>
2022-04-08 11:35:27 +0300

    Еще

--- static/js/navigatorPower.js
+++ static/js/navigatorPower.js
@@ -1,4 +1,4 @@
 function navigator_1_power (){
-    product = '4919A2A3-290F-419F-A45C-E0DC75F91DB3';
-    window.localStorage['product'] = product;
+    local_data = 'n1';
+    window.localStorage['local_data '] = local_data ;
 };
