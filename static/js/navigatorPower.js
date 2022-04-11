585d50dbdf3ed49f7e59f00dbee5ed222647b7fc
BookLove <krouvy@mail.ru>
2022-04-11 16:31:02 +0300

    navigatorPower golova 2

--- static/js/navigatorPower.js
+++ static/js/navigatorPower.js
@@ -2,3 +2,9 @@ function navigator_1_power (){
     local_data = 'n1';
     window.localStorage['local_data '] = local_data ;
 };
+
+function navigator_2_golova_2_power(){
+    local_data = 'n2_2';
+    window.localStorage['local_data '] = local_data ;
+};
+
