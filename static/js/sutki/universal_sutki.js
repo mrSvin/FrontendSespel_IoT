882dcd3cffc7cf18056a3e807996b8e00fe45c89
BookLove <krouvy@mail.ru>
2022-05-16 17:19:58 +0300

    Нука

--- static/js/sutki/universal_sutki.js
+++ static/js/sutki/universal_sutki.js
@@ -378,6 +378,10 @@ function buildRoundDiagram(roundDiagram, exception, index, startContainer) {
     // Если массив пустой или нулевой, то
     if ((roundDiagram == 0) || (roundDiagram === null)) {
 
+        roundDiagram.forEach((elem, index)=>{
+            roundDiagram[index] = +elem
+        })
+
         // Переменная выполненности исключения, меняется на 1 если произошло исключение
         var exceptionGoted = 0
 
