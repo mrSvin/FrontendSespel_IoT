7e308a8ff860190c22381bf7c50c50517c7261fd
BookLove <krouvy@mail.ru>
2022-07-22 09:19:34 +0300

    css

--- static/js/solutions.js
+++ static/js/solutions.js
@@ -155,7 +155,7 @@ function convertDaysToSmena(today, yesterday, calendarDate=null) {
     let programName2 = []
 
     // Переменная с индексом первого элемента следующего дня имени программы
-    let programName2Index = today.programName.length
+    let programName2Index = today[5].length
 
     // Получении объекта с именем текущего станка
     // Хранящий объедененные массивы текущего и предыдущего дня
