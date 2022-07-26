6fbf330ba09426f35e5f22a0f4f2987d94b122e3
VasilievAV <alva@sespel.com>
2022-07-26 09:40:40 +0300

    commit

--- static/js/fetchRequests.js
+++ static/js/fetchRequests.js
@@ -14,7 +14,7 @@ function fetchRequest(dateCalendar, complexObject, complexName) {
         })
 }
 
-function fetchRequestBuildHC(dateCalendar, complexObject, complexName, idContainer) {
+function fetchRequestBuildHC(dateCalendar, complexObject, complexName, idContainer, nagruzkaType = 'Нагрузка', typeLine = "multiLine") {
     return fetch(`/api/complexData/${complexName}_days_date:${dateCalendar}`, {method: 'GET'})
         .then((response) => response.json())
         .then((data) => {
@@ -26,19 +26,25 @@ function fetchRequestBuildHC(dateCalendar, complexObject, complexName, idContain
             complexObject.roundArray = data.roundData
             complexObject.programName = data.programName
 
-            let convertDataRuchnoi = parseLinearSutki(complexObject.ruchnoyArray, 0, dateCalendar)
-            let convertDataWork = parseLinearSutki(complexObject.workArray, 1, dateCalendar,complexObject.programName)
-            let convertDataPause = parseLinearSutki(complexObject.pauseArray, 2, dateCalendar)
-            let convertDataOff = parseLinearSutki(complexObject.offArray, 3, dateCalendar)
-            let convertDataAvar = parseLinearSutki(complexObject.avarArray, 4, dateCalendar)
-            highChartSutkiLine(convertDataWork, convertDataPause, convertDataOff, convertDataAvar, convertDataRuchnoi, "Нагрузка",idContainer)
+            let arrayLine;
+            if (typeLine == 'multiLine') {
+                arrayLine =[0,1,2,3,4]
+            } else {
+                arrayLine =[0,0,0,0,0]
+            }
+            let convertDataRuchnoi = parseLinearSutki(complexObject.ruchnoyArray, arrayLine[0], dateCalendar)
+            let convertDataWork = parseLinearSutki(complexObject.workArray, arrayLine[1], dateCalendar,complexObject.programName)
+            let convertDataPause = parseLinearSutki(complexObject.pauseArray, arrayLine[2], dateCalendar)
+            let convertDataOff = parseLinearSutki(complexObject.offArray, arrayLine[3], dateCalendar)
+            let convertDataAvar = parseLinearSutki(complexObject.avarArray, arrayLine[4], dateCalendar)
+            highChartSutkiLine(convertDataWork, convertDataPause, convertDataOff, convertDataAvar, convertDataRuchnoi, nagruzkaType,idContainer)
 
             let workRound = parseInt(complexObject.roundArray[0]);
             let passRound = parseInt(complexObject.roundArray[1]);
             let offRound = parseInt(complexObject.roundArray[2]);
             let avarRound = parseInt(complexObject.roundArray[3]);
             let nagruzkaRound = parseInt(complexObject.roundArray[4]);
-            highChartRound(workRound, passRound, offRound, avarRound, nagruzkaRound, 'Нагрузка', idContainer)
+            highChartRound(workRound, passRound, offRound, avarRound, nagruzkaRound, nagruzkaType, idContainer)
 
             return complexObject
         })
@@ -56,9 +62,9 @@ function fetchMonthHighCharts(complexName, dateInput, idContainer) {
 }
 
 function fetchRequestServiceInfo(complexName) {
-
-    let url = `http://192.168.3.41:8086/api/serviceInfo/${complexName}`
-
+    let serverDomain=window.location.hostname
+    let url = `http://${serverDomain}:8086/api/serviceInfo/${complexName}`
+    console.log(url)
     return fetch(url, {method: 'GET'})
         .then((response) => response.json())
         .then((data) => {
@@ -66,7 +72,7 @@ function fetchRequestServiceInfo(complexName) {
         })
 }
 
-function fetchRequestAddService(userName, userRole,complexName, infoWorks, periodService,setFormAddService) {
+function fetchRequestAddService(userName, userRole,complexName, infoWorks, periodService,setFormAddService, errorService, setErrorService) {
     let myHeaders = new Headers();
     myHeaders.append("Content-Type", "application/json");
 
@@ -83,34 +89,37 @@ function fetchRequestAddService(userName, userRole,complexName, infoWorks, perio
         body: raw,
         redirect: 'follow'
     };
+    if ( userRole == "ROLE_SERVICE" || userRole == "ROLE_ADMIN" && infoWorks.length>10) {
+        let serverDomain=window.location.hostname
+        fetch(`http://${serverDomain}:8086/api/addService`, requestOptions)
+            .then(response => response.text())
+            .then(result => {
+                console.log('Запрос прошел', result)
+                if (result === 'ok') {
+                    let newDataTable = setFormAddService.dataService;
 
-    fetch("http://192.168.3.41:8086/api/addService", requestOptions)
-        .then(response => response.text())
-        .then(result => {
-            console.log('Запрос прошел', result)
-            if (result === 'ok') {
-                let newDataTable = setFormAddService.dataService;
+                    if (newDataTable.length !== 0) {
+                        newDataTable.unshift({info_works: infoWorks, user_name:userName, time_service:getTimeToday(), period_service:msToTimeDays(new Date(getTimeToday()) - new Date(newDataTable[0].time_service))})
+                    }
+                    else newDataTable.unshift({info_works: infoWorks, user_name:userName, time_service:getTimeToday(), period_service:'-'})
 
-                if (newDataTable.length !== 0) {
-                    newDataTable.unshift({info_works: infoWorks, user_name:userName, time_service:getTimeToday(), period_service:msToTimeDays(new Date(getTimeToday()) - new Date(newDataTable[0].time_service))})
+                    console.log(newDataTable)
+                    setFormAddService.setDataService(newDataTable)
+                    setFormAddService.setFormAddService(false)
+
+                    let allServiceArray = newDataTable.map(e=> {
+                        return e.time_service
+                    })
+
+                    highChartServiceHistory(allServiceArray.reverse())
+                    highChartServiceNow(allServiceArray.reverse(),periodService)
                 }
-                else newDataTable.unshift({info_works: infoWorks, user_name:userName, time_service:getTimeToday(), period_service:'-'})
+            })
+            .catch(error => alert('Ошибка при отправке запроса', error));
+    } else if (infoWorks.length<=10) {
+        setErrorService("Сообщение о проведенных работах слишком короткое.")
+    } else {
+        setErrorService("Недостаточно прав. Для осуществления операции вы должны обладать правами \"Сервис\" или \"Администратор\".")
+    }
 
-              console.log(newDataTable)
-                setFormAddService.setDataService(newDataTable)
-                setFormAddService.setFormAddService(false)
-
-                let allServiceArray = newDataTable.map(e=> {
-                    return e.time_service
-                })
-
-                let info = newDataTable.map(e=> {
-                    return [e.user_name, e.info_works]
-                })
-
-                highChartServiceHistory(allServiceArray.reverse(), info.reverse())
-                highChartServiceNow(allServiceArray.reverse(),periodService)
-            }
-        })
-        .catch(error => console.log('Ошибка, недостаточно прав', error));
 }
