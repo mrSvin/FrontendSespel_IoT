20c5ae5c0f324bdf00dda86194934df5458d3f54
BookLove <krouvy@mail.ru>
2022-08-04 11:01:43 +0300

    Еще

--- static/js/components/service.jsx
+++ static/js/components/service.jsx
@@ -53,22 +53,19 @@ function Service() {
                 return e.time_service
             })
 
-            let lastPeriod = value.map(e=> {
-                return e.timeNext
-            })
-
-            lastPeriod = lastPeriod[0];
-
             ArrayPeriod = getArrayPeriodsBetween(ArrayPeriod)
 
             let dataTable = [];
             value.forEach((e,i) => {
-                //e.period_service = ArrayPeriod[i]
+                e.timeNext = e.period_service
+                e.period_service = ArrayPeriod[i]
                 dataTable.push(e)
             })
 
-           setDataService(dataTable)
+            setDataService(dataTable)
 
+            
+            let lastPeriod = value[0].timeNext
             let allServiceArray = [] //'2022-07-22 07:08:41', '2022-07-22 07:12:41', '2022-07-22 07:17:40', '2022-07-22 07:21:51', '2022-07-22 08:33:03', '2022-07-22 09:36:36', '2022-07-22 09:57:11', '2022-07-22 09:58:52']
             value.forEach(element => allServiceArray.push(element.time_service) )
 
