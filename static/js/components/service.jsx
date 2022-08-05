f5a613c1d9f3f123c8a3ef6f45e1710a73af3d9a
BookLove <krouvy@mail.ru>
2022-08-05 16:24:37 +0300

    картинки

--- static/js/components/service.jsx
+++ static/js/components/service.jsx
@@ -11,19 +11,21 @@ function Service() {
     useEffect(() => {
 
         let objectImg = {
-            'apec':'apec.png', 'ar55':'arr55.png', 'crysta-apex':'crystal_apex',
+            'apec':'apec.png', 'ar55':'ar55.png', 'crysta-apex':'crystal_apex.png',
             '?***?':'dmf.png', 'ctx310':'dmg_ctx310.png', 'ctx510':'dmg_ctx510.png',
             'ctx650':'dmg_ctx650.png', 'dmc1035':'dmg_dmc1035.png', 'dmf260':'dmg_dmf260.png',
-            'dmu50':'dmg_dmu50.png', 'epp':'epp.png', 'faccin':'faccin.png',
-            '?? faccin ??':'faccin_2.png', 'gamma2000':'gamma2000.png', 'kometa':'kometa.png',
-            'навигатор':'navigator.png', 'nk600':'nk600.png', 'nlx3000':'NLX3000.png',
-            'ntx1000':'ntx1000.png', 'печь':'pech.png', 'press':'press.png',
-            'стп':'progress.png', 'макс':'robot.png', 'm710':'robot.png',
-            'rtk12C':'robot.png', 'крот':'robot.png', 'пранс':'robot.png',
+            'dmu50':'dmg_dmu50.png', 'ЭПП':'epp.png', 'faccin 4':'faccin.png',
+            'faccin 10':'faccin_2.png', 'gamma2000':'gamma2000.png', 'комета':'kometa.png',
+            'навигатор':'navigator.png', 'нк600':'nk600.png', 'nlx3000':'NLX3000.png',
+            'ntx1000':'ntx1000.png', 'печь':'pech.png', 'Пресс':'press.png',
+            'стп':'progress.png', 'макс':'robot.png', 'м710':'robot.png',
+            'РТК12C':'robot.png', 'крот':'robot.png', 'пранс':'robot.png',
             'p250':'robot_p250.png', 'sk50':'sk50.png', 'склад':'sklad.png',
-            'stp13m':'stp13m.png', 'trulaser':'trulaser.png',
-            'uvf':'uvf_1_2.png', 'uvf5220':'UVF_5220.png',
+            'СТП13М':'stp13m.png', 'trulaser':'trulaser.png',
+            'увф':'uvf_1_2.png', 'уф5220':'UVF_5220.png',
         }
+
+        let exceptions = ['faccin']
 
         let nameToFetch = parseNameUrl(document.location.pathname);
         setNameComplex(nameToFetch)
@@ -35,8 +37,12 @@ function Service() {
         keys.forEach(e=>{
             let inUrl = nameToFetch
             inUrl = (inUrl.split(' ')[0]).toLowerCase();
-            if (e.includes(inUrl)){
+            if (e.includes(inUrl) && exceptions.includes(!inUrl)){
                 img = objectImg[inUrl];
+                img = `../images/${img}`
+            }
+             else  if (e.includes(inUrl) && exceptions.includes(inUrl)){
+                img = objectImg[nameToFetch];
                 img = `../images/${img}`
             }
         })
