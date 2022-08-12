21161fc4cc54520c7520b29b97bcf1ed9162564d
BookLove <krouvy@mail.ru>
2022-08-12 10:58:20 +0300

    Пуш

--- static/js/components/report.jsx
+++ static/js/components/report.jsx
@@ -1,17 +1,4 @@
-function changeData(){
-    let dataReport = [
-        {'number':2,'numChertIz':'495123525001','numIz':121350,
-            'needForce':353333, 'factForce1':22, 'factForce2':3531, 'maxDeform':93.5,
-            'leftDeform':12.1, 'godnost':'Годен', 'author':'Буклов А.В.', 'date':'10.08.2022'},
-        {'number':3,'numChertIz':'C435064S-5.0301','numIz':41,
-            'needForce':24, 'factForce1':11.3, 'factForce2':33.2, 'maxDeform':100.0,
-            'leftDeform':0.9, 'godnost':'Не годен', 'author':'Буклов А.В.', 'date':'11.08.2022'}
-    ]
-    setDataReportState(dataReport)
-}
-
 function Report() {
-
     let dataReport = [
         {'number':0,'numChertIz':'4977.06.008-5001','numIz':10,
             'needForce':3.333, 'factForce1':3.2, 'factForce2':3.1, 'maxDeform':99.5,
@@ -23,13 +10,25 @@ function Report() {
 
     let [stendName, setStendName] = useState('неизвестно')
 
-    let [dataReportState, setDataReportState] = useState('неизвестно')
+    let [dataReportState, setDataReportState] = useState('1')
 
     useEffect(() => {
         let nameToFetch = parseNameUrl(document.location.pathname);
         setStendName(nameToFetch)
         setDataReportState(dataReport)
         })
+
+    function changeData(){
+        let dataReport = [
+            {'number':2,'numChertIz':'495123525001','numIz':121350,
+                'needForce':353333, 'factForce1':22, 'factForce2':3531, 'maxDeform':93.5,
+                'leftDeform':12.1, 'godnost':'Годен', 'author':'Буклов А.В.', 'date':'10.08.2022'},
+            {'number':3,'numChertIz':'C435064S-5.0301','numIz':41,
+                'needForce':24, 'factForce1':11.3, 'factForce2':33.2, 'maxDeform':100.0,
+                'leftDeform':0.9, 'godnost':'Не годен', 'author':'Буклов А.В.', 'date':'11.08.2022'}
+        ]
+        setDataReportState(dataReport)
+    }
 
 
     return(
@@ -61,6 +60,7 @@ function Report() {
 }
 
 function TableReportBody({data}) {
+    console.log('Логика для длины...')
     return (
     <tbody>
     {data.map((val,i) => {
