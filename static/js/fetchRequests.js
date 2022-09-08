37dd4480e90d8758123c308ee25e87d69183b033
BookLove <krouvy@mail.ru>
2022-09-08 10:25:55 +0300

    Проверка

--- static/js/fetchRequests.js
+++ static/js/fetchRequests.js
@@ -23,16 +23,16 @@ function fetchRequestBuildHC(dateCalendar, complexObject, complexName, idContain
 
             let arrayLine;
             if (typeLine == 'multiLine') {
-                arrayLine =[0,1,2,3,4]
+                arrayLine = [0, 1, 2, 3, 4]
             } else {
-                arrayLine =[0,0,0,0,0]
+                arrayLine = [0, 0, 0, 0, 0]
             }
             let convertDataRuchnoi = parseLinearSutki(complexObject.ruchnoyArray, arrayLine[0], dateCalendar)
-            let convertDataWork = parseLinearSutki(complexObject.workArray, arrayLine[1], dateCalendar,complexObject.programName)
+            let convertDataWork = parseLinearSutki(complexObject.workArray, arrayLine[1], dateCalendar, complexObject.programName)
             let convertDataPause = parseLinearSutki(complexObject.pauseArray, arrayLine[2], dateCalendar)
             let convertDataOff = parseLinearSutki(complexObject.offArray, arrayLine[3], dateCalendar)
             let convertDataAvar = parseLinearSutki(complexObject.avarArray, arrayLine[4], dateCalendar)
-            highChartSutkiLine(convertDataWork, convertDataPause, convertDataOff, convertDataAvar, convertDataRuchnoi, nagruzkaType,idContainer)
+            highChartSutkiLine(convertDataWork, convertDataPause, convertDataOff, convertDataAvar, convertDataRuchnoi, nagruzkaType, idContainer)
 
             let workRound = parseInt(complexObject.roundArray[0]);
             let passRound = parseInt(complexObject.roundArray[1]);
@@ -51,13 +51,13 @@ function fetchMonthHighCharts(complexName, dateInput, idContainer) {
         .then((data) => {
             highChartMonthLine(data.work, data.pause, data.off, data.avar, data.nagruzka, 'Нагрузка', idContainer)
             highChartRound(averageMonthdata(data.work), averageMonthdata(data.pause), averageMonthdata(data.off),
-                averageMonthdata(data.avar),  averageMonthdata(data.nagruzka), 'Нагрузка', idContainer)
+                averageMonthdata(data.avar), averageMonthdata(data.nagruzka), 'Нагрузка', idContainer)
             return data
         })
 }
 
 function fetchRequestServiceInfo(complexName) {
-    let serverDomain=window.location.hostname
+    let serverDomain = window.location.hostname
     let url = `http://${serverDomain}:8086/api/serviceInfo/${complexName}`
     console.log(url)
     return fetch(url, {method: 'GET'})
@@ -67,11 +67,11 @@ function fetchRequestServiceInfo(complexName) {
         })
 }
 
-function fetchRequestAddService(userName, userRole,complexName, infoWorks, periodService,setFormAddService, errorService, setErrorService) {
+function fetchRequestAddService(userName, userRole, complexName, infoWorks, periodService, setFormAddService, errorService, setErrorService) {
     let myHeaders = new Headers();
     myHeaders.append("Content-Type", "application/json");
 
-    infoWorks = infoWorks.replaceAll(`'`, '').replaceAll(`"`,'').replaceAll(`{`,'').replaceAll(`}`,'')
+    infoWorks = infoWorks.replaceAll(`'`, '').replaceAll(`"`, '').replaceAll(`{`, '').replaceAll(`}`, '')
 
     let raw = JSON.stringify({
         "complexName": complexName,
@@ -86,37 +86,65 @@ function fetchRequestAddService(userName, userRole,complexName, infoWorks, perio
         body: raw,
         redirect: 'follow'
     };
-    if ( userRole == "ROLE_SERVICE" || userRole == "ROLE_ADMIN" && infoWorks.length>10) {
-        let serverDomain=window.location.hostname
-        fetch(`http://${serverDomain}:8086/api/addService`, requestOptions)
+    if (userRole == "ROLE_SERVICE" || userRole == "ROLE_ADMIN" && infoWorks.length > 10) {
+        let serverDomain = window.location.hostname
+        let serverPort = window.location.port
+        let port = 17086
+
+        if (serverDomain != 'iot.sespel.com') {
+            if(serverDomain == '89.151.134.234'){
+                if(serverPort == '16080'){
+                    port = 16086
+                }
+                else port = 17086
+            }
+            else if(serverDomain == '192.168.3.41'){
+                port = 16086
+            }
+            else port = 17086
+        }
+
+
+        fetch(`http://${serverDomain}:${port}/api/addService`, requestOptions)
             .then(response => response.text())
             .then(result => {
                 if (result === 'ok') {
                     let newDataTable = setFormAddService.dataService;
 
                     if (newDataTable.length !== 0) {
-                        newDataTable.unshift({info_works: infoWorks, user_name:userName, time_service:dayTimeNow(), period_service:msToTimeDays(new Date(dayTimeNow()) - new Date(newDataTable[0].time_service),365), timeNext:periodService})
-                    }
-                    else newDataTable.unshift({info_works: infoWorks, user_name:userName, time_service:dayTimeNow(), period_service:'-', timeNext:periodService})
+                        newDataTable.unshift({
+                            info_works: infoWorks,
+                            user_name: userName,
+                            time_service: dayTimeNow(),
+                            period_service: msToTimeDays(new Date(dayTimeNow()) - new Date(newDataTable[0].time_service), 365),
+                            timeNext: periodService
+                        })
+                    } else newDataTable.unshift({
+                        info_works: infoWorks,
+                        user_name: userName,
+                        time_service: dayTimeNow(),
+                        period_service: '-',
+                        timeNext: periodService
+                    })
 
-                    
+
                     setFormAddService.setDataService(newDataTable)
                     setFormAddService.setFormAddService(false)
 
-                    let allServiceArray = newDataTable.map(e=> {
+                    let allServiceArray = newDataTable.map(e => {
                         return e.time_service
                     })
 
-                    let info = newDataTable.map(e=> {
+                    let info = newDataTable.map(e => {
                         return [e.user_name, e.info_works]
                     })
 
                     highChartServiceHistory(allServiceArray.reverse(), info.reverse())
-                    highChartServiceNow(allServiceArray.reverse(),periodService)
+                    highChartServiceNow(allServiceArray.reverse(), periodService)
                 }
             })
-            .catch(error =>console.log('Ошибка при отправке запроса', error));
-    } else if (infoWorks.length<=10) {
+            .catch(error => console.log('Ошибка при отправке запроса', error));
+    } else if (infoWorks.length <= 10) {
         setErrorService("Сообщение о проведенных работах слишком короткое.")
     } else {
         setErrorService("Недостаточно прав. Для осуществления операции вы должны обладать правами \"Сервис\" или \"Администратор\".")
@@ -134,22 +162,22 @@ function fetchRequestSmena(dateCalendar, complexName) {
 }
 
 function fetchRequestCurrent(complexName, setDataReportState) {
-    let serverDomain=window.location.hostname
+    let serverDomain = window.location.hostname
     let url = `http://${serverDomain}:8087/api/${complexName}`
 
     fetch(url, {method: 'GET'})
         .then((response) => response.json())
         .then((data) => {
-            let Name = data.actNum == '1'? '4977.06.008-5001': 'C435064S-5.0301'
+            let Name = data.actNum == '1' ? '4977.06.008-5001' : 'C435064S-5.0301'
             let date = (new Date(data.lastRequest).getTime() + 10800000)
             date = new Date(date).toISOString().slice(0, 10) + ' ' + new Date(date).toISOString().slice(11, 19)
 
             let dataReport = [data.requestWriteDB, data.prodNum, data.actNum, data.resultR, Name, data.authorId, data.workMode, data.maxDeformation, data.ostDeformation, data.actForce1R, data.actForce2R, data.needForce, date]
             setDataReportState([
                 [
-                    'Регистр состояния записи на сервер','Номер изделия', 'Номер акта', 'Результат',
+                    'Регистр состояния записи на сервер', 'Номер изделия', 'Номер акта', 'Результат',
                     'Номер чертежа', 'ID автора', 'Состояние работы', 'Максимальная деформация',
-                    'Остаточная деформация','Фактическая сила (датчик 1), Т', 'Фактическая сила (датчик 2), Т',
+                    'Остаточная деформация', 'Фактическая сила (датчик 1), Т', 'Фактическая сила (датчик 2), Т',
                     'Требуемая сила (датчик 2), Т', 'Последнее время получения данных с оборудования'
                 ],
                 dataReport])
@@ -158,14 +186,14 @@ function fetchRequestCurrent(complexName, setDataReportState) {
 
 function fetchRequestReport(complexName, setDataReportState) {
 
-    let serverDomain=window.location.hostname
+    let serverDomain = window.location.hostname
     let url = `http://${serverDomain}:8087/api/${complexName}`
 
     fetch(url, {method: 'GET'})
         .then((response) => response.json())
         .then((data) => {
-            let dataReport = data.map(e=>{
-                let fix = e.numberDrawing == '1'? '4977.06.008-5001': 'C435064S-5.0301'
+            let dataReport = data.map(e => {
+                let fix = e.numberDrawing == '1' ? '4977.06.008-5001' : 'C435064S-5.0301'
                 return [e.numberAct, fix, e.numberProduct, e.requiredForce, e.actualForce, e.maxDeformation, e.ostDeformation, e.valid, e.author, e.dateTime]
             })
             setDataReportState(dataReport)
