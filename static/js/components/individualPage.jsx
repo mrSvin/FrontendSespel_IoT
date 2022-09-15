6c116d91bf60c5d29ced24bfe2941b2edfbad7ce
BookLove <krouvy@mail.ru>
2022-09-15 18:13:13 +0300

    Чек

--- static/js/components/individualPage.jsx
+++ static/js/components/individualPage.jsx
@@ -2,8 +2,8 @@ function IndividualPageInfo() {
 
     function changeMainList(mainList, selectedObjects) {
         let index = 0
-        for(let i=0; i<mainList.length; i++){
-            for (let j=0; j<mainList[i][1]; j++){
+        for (let i = 0; i < mainList.length; i++) {
+            for (let j = 0; j < mainList[i][1]; j++) {
                 selectedObjects[index] = mainList[i][0]
                 index++
             }
@@ -18,18 +18,18 @@ function IndividualPageInfo() {
             ["НК600", "НК600"],
         ],
         ['Мех.уч.2.пл',
-            ["УФ5220","УФ5220"],
+            ["УФ5220", "УФ5220"],
             ["СТП Сеспель", 'СТП Сеспель'],
             ["NTX1000 2", "NTX1000 2"],
-            ["SK50",  'SK50'],
+            ["SK50", 'SK50'],
             ["APEC", "APEC"],
             ["DMU50 3", "DMU50 3"],
             ["DMU50 4", "DMU50 4"],
-            ["CTX310 2",  'CTX310 2', 'CTX310 2'],
+            ["CTX310 2", 'CTX310 2', 'CTX310 2'],
             ["CTX510 2", "CTX510 2"],
             ["CTX510 3", "CTX510 3"],
             ["CTX310 3", "CTX310 3"],
-            ["CTX510 4",  'CTX510 4'],
+            ["CTX510 4", 'CTX510 4'],
             ["CTX510 5", "CTX510 5"],
             ["DMC1035 2", "DMC1035 2"],
             ["DMU50 5", "DMU50 5"],
@@ -38,27 +38,27 @@ function IndividualPageInfo() {
             ["AR55", "AR55"],
         ],
         ['Резка',
-            ["Навигатор 1","Навигатор 1",  null,"Навигатор 1", "Навигатор 1"],
+            ["Навигатор 1", "Навигатор 1", null, "Навигатор 1", "Навигатор 1"],
             ["Навигатор 2", 'Навигатор 2', null, "Навигатор 2", "Навигатор 2"],
             ["Навигатор 3", "Навигатор 3", null, "Навигатор 3", "Навигатор 3"],
-            ["TruLaser",  'TruLaser'],
+            ["TruLaser", 'TruLaser'],
             ["Комета 1", "Комета 1"],
             ["Комета 2", "Комета 2"],
             ["Комета 3", "Комета 3"],
         ],
         ['Мех.уч.1.пл',
-            ["УВФ-1 1","УВФ-1 1"],
-            ["УВФ-1 2","УВФ-1 2"],
-            ["NTX1000","NTX1000","NTX1000"],
-            ["NLX3000","NLX3000","NLX3000"],
-            ["GAMMA2000","GAMMA2000","GAMMA2000"],
-            ["CTX650","CTX650","CTX650"],
-            ["DMF260","DMF260","DMF260"],
-            ["DMU50 1","DMU50 1","DMU50 1"],
-            ["DMU50 2","DMU50 2","DMU50 2"],
-            ["DMC1035","DMC1035","DMC1035"],
-            ["CTX310 1","CTX310 1","CTX310 1"],
-            ["CTX510 1","CTX510 1","CTX510 1"],
+            ["УВФ-1 1", "УВФ-1 1"],
+            ["УВФ-1 2", "УВФ-1 2"],
+            ["NTX1000", "NTX1000", "NTX1000"],
+            ["NLX3000", "NLX3000", "NLX3000"],
+            ["GAMMA2000", "GAMMA2000", "GAMMA2000"],
+            ["CTX650", "CTX650", "CTX650"],
+            ["DMF260", "DMF260", "DMF260"],
+            ["DMU50 1", "DMU50 1", "DMU50 1"],
+            ["DMU50 2", "DMU50 2", "DMU50 2"],
+            ["DMC1035", "DMC1035", "DMC1035"],
+            ["CTX310 1", "CTX310 1", "CTX310 1"],
+            ["CTX510 1", "CTX510 1", "CTX510 1"],
         ],
         ['Роботы',
             ["МАКС 1", "МАКС 1", null, "МАКС 1"],
@@ -70,27 +70,27 @@ function IndividualPageInfo() {
             ["ПРАНС", "ПРАНС", null, "ПРАНС"],
         ],
         ['Спец. Комплексы',
-            ["Пресс ЧПУ для ступиц","Пресс ЧПУ для ступиц"],
-            ["ЭПП","ЭПП"],
-            ["СТП13М","СТП13М"],
-            ['Стенд для ресурсных испытаний','Стенд для ресурсных испытаний', null, null, null, 'Стенд','Стенд'],
+            ["Пресс ЧПУ для ступиц", "Пресс ЧПУ для ступиц"],
+            ["ЭПП", "ЭПП"],
+            ["СТП13М", "СТП13М"],
+            ['Стенд для ресурсных испытаний', 'Стенд для ресурсных испытаний', null, null, null, 'Стенд', 'Стенд'],
         ],
         ['Склады',
-            ["Склад Мех. цеха","Склад Мех. цеха"],
+            ["Склад Мех. цеха", "Склад Мех. цеха"],
         ],
         ['Литейка',
             ["Печь Индукционная", "Печь Индукционная"],
         ],
         ['Гибка',
-            ["FACCIN 4","FACCIN 4"],
-            ["FACCIN 10","FACCIN 10"],
+            ["FACCIN 4", "FACCIN 4"],
+            ["FACCIN 10", "FACCIN 10"],
         ],
     ]
     let complexNameOld = []
 
-    complexName.forEach(e=>{
+    complexName.forEach(e => {
         let array = e.slice(1)
-        array.forEach(i=>{
+        array.forEach(i => {
             complexNameOld.push(i)
         })
     })
@@ -98,9 +98,9 @@ function IndividualPageInfo() {
     let complexImg = [
         "../images/crystal_apex.png", "../images/nk600.png", "../images/UVF_5220.png",
         "../images/progress.png", "../images/ntx1000.png", "../images/sk50.png",
-        "../images/apec.png", "../images/dmg_dmu50.png","../images/dmg_dmu50.png",
+        "../images/apec.png", "../images/dmg_dmu50.png", "../images/dmg_dmu50.png",
         "../images/dmg_ctx310.png", "../images/dmg_ctx510.png", "../images/dmg_ctx510.png",
-        "../images/dmg_ctx310.png","../images/dmg_ctx510.png", "../images/dmg_ctx510.png",
+        "../images/dmg_ctx310.png", "../images/dmg_ctx510.png", "../images/dmg_ctx510.png",
         "../images/dmg_dmc1035.png", "../images/dmg_dmu50.png", "../images/dmg_dmu50.png",
         "../images/dmg_dmu50.png", "../images/ar55.png", "../images/navigator.png",
         "../images/navigator.png", "../images/navigator.png", "../images/trulaser.png",
@@ -110,21 +110,21 @@ function IndividualPageInfo() {
         "../images/dmg_dmf260.png", "../images/dmg_dmu50.png", "../images/dmg_dmu50.png",
         "../images/dmg_dmc1035.png", "../images/dmg_ctx310.png", "../images/dmg_ctx510.png",
         "../images/robot.png", "../images/robot.png", "../images/robot.png", "../images/robot.png",
-        "../images/robot_p250.png", "../images/robot.png", "../images/robot.png","../images/press.png",
+        "../images/robot_p250.png", "../images/robot.png", "../images/robot.png", "../images/press.png",
         "../images/epp.png", "../images/stp13m.png", "../images/stendResource.png", "../images/sklad.png",
         "../images/pech.png", "../images/faccin.png", "../images/faccin_2.png",
     ]
 
     let complexRequest = [
-        'kim', 'nk600','uvf5220', 'progress', 'ntx1000_2', 'sk50', 'apec',
+        'kim', 'nk600', 'uvf5220', 'progress', 'ntx1000_2', 'sk50', 'apec',
         'dmg_dmu50_3', 'dmg_dmu50_4', 'dmg_ctx310_2', 'dmg_ctx510_2', 'dmg_ctx510_3',
-        'dmg_ctx310_3','dmg_ctx510_4', 'dmg_ctx510_5', 'dmg_dmc1035_2','dmg_dmu50_5',
+        'dmg_ctx310_3', 'dmg_ctx510_4', 'dmg_ctx510_5', 'dmg_dmc1035_2', 'dmg_dmu50_5',
         'dmg_dmu50_6', 'dmg_dmu50_7', 'ar55', 'navigator_1', 'navigator_2_golova_2',
         'navigator_3', 'trulaser', 'kometa_1', 'kometa_2', 'kometa_3', 'uvf_1', 'uvf_2',
         'ntx1000', 'nlx3000', 'dmg_gamma2000', 'dmg_ctx650', 'dmg_dmf260', 'dmg_dmu50_1',
-        'dmg_dmu50_2', 'dmg_dmc1035', 'dmg_ctx310_1','dmg_ctx510_1','maks_1', 'maks_2', 'm710',
+        'dmg_dmu50_2', 'dmg_dmc1035', 'dmg_ctx310_1', 'dmg_ctx510_1', 'maks_1', 'maks_2', 'm710',
         'rtk12c', 'p250', 'krot', 'prans', 'press', 'epp', 'stp13m', 'stend_resources', 'sclad_meh',
-        'pech_nerg', 'faccin_1','faccin_2',
+        'pech_nerg', 'faccin_1', 'faccin_2',
     ]
 
     let buttonsVrs = [
@@ -207,29 +207,35 @@ function IndividualPageInfo() {
     // Состояние переменной мульти Диаграммы
     let [stateLineHC, setStateLineHC] = useState("multiLine");
 
-    if(localStorage['selectedObjects'] == undefined) {
+    if (localStorage['selectedObjects'] == undefined) {
         localStorage['selectedObjects'] = new Array(complexRequest.length).fill(false)
     }
 
-    if(localStorage['selectedCategory'] == undefined) {
+    if (localStorage['selectedCategory'] == undefined) {
         localStorage['selectedCategory'] = new Array(complexName.length).fill(false)
     }
 
     // Состояния чекбоксов станков
     let [selectedCategory, setSelectedCategory] = useState(
-        (localStorage['selectedCategory'] == undefined)? new Array(complexName.length).fill(false) :window.localStorage['selectedCategory'].split(',').map(e=>{
-            return (e =='true')
+        (localStorage['selectedCategory'] == undefined) ? new Array(complexName.length).fill(false) : window.localStorage['selectedCategory'].split(',').map(e => {
+            return (e == 'true')
         })
     );
 
     // Состояния чекбоксов станков
     let [selectedObjects, setSelectedObjects] = useState(
-        (localStorage['selectedObjects'] == undefined)? new Array(complexRequest.length).fill(false) :window.localStorage['selectedObjects'].split(',').map(e=>{return (e=='true')})
+        (localStorage['selectedObjects'] == undefined) ? new Array(complexRequest.length).fill(false) : window.localStorage['selectedObjects'].split(',').map(e => {
+            return (e == 'true')
+        })
     );
 
-    let [valuesState, setValuesState] = useState(selectedObjects.map((item, index) => {return true === item ? values[index] : null;}).filter(e=> e!=null))
+    let [valuesState, setValuesState] = useState(selectedObjects.map((item, index) => {
+        return true === item ? values[index] : null;
+    }).filter(e => e != null))
 
-    let [valuesStateWait, setValuesStateWait] = useState(selectedObjects.map((item, index) => {return true === item ? values[index] : null;}).filter(e=> e!=null))
+    let [valuesStateWait, setValuesStateWait] = useState(selectedObjects.map((item, index) => {
+        return true === item ? values[index] : null;
+    }).filter(e => e != null))
 
     const [isActive, setActive] = useState(false);
 
@@ -253,8 +259,8 @@ function IndividualPageInfo() {
         window.localStorage['selectedCategory'] = updatedCheckedState
         setSelectedCategory(updatedCheckedState)
 
-        let mainList = complexName.map((e, i)=>{
-            return [updatedCheckedState[i], e.length-1]
+        let mainList = complexName.map((e, i) => {
+            return [updatedCheckedState[i], e.length - 1]
         })
 
         changeMainList(mainList, selectedObjects)
@@ -349,71 +355,76 @@ function IndividualPageInfo() {
                     <span onClick={toggleClass}>Выбор оборудования</span>
                     <div className="listComplex">
                         <span>▼</span>
-                        <ul className='toppings-list'
-                            className={isActive ? 'toppings-list toppings-list-visible' : 'toppings-list'}>
+                        <div
+                            className={isActive ? 'containerStanokList' : 'containerStanokList containerStanokListHidden'}>
+                            <ul className='toppings-list'
+                                className={isActive ? 'toppings-list toppings-list-visible' : 'toppings-list'}>
 
-                            {complexName.map((razdel, index)=>{
-                                let copy = razdel.slice(1)
+                                {complexName.map((razdel, index) => {
+                                    let copy = razdel.slice(1)
 
-                                let paddingNow = {
-                                    paddingBottom:  30 * copy.length + 6
-                                };
+                                    let paddingNow = {
+                                        paddingBottom: 30 * copy.length + 6
+                                    };
 
-                                console.log('Этот паддинг = ',paddingNow)
+                                    console.log('Этот паддинг = ', paddingNow)
 
-                                // function HelloWorldComponent() {
-                                //     return <div style={paddingNow}>Привет, мир!</div>;
-                                // }
+                                    // function HelloWorldComponent() {
+                                    //     return <div style={paddingNow}>Привет, мир!</div>;
+                                    // }
 
-                                return (
-                                    <li key={index}>
-                                        <div className="toppings-list-item">
-                                            <div className="left-section">
-                                                <input
-                                                    type="checkbox"
-                                                    id={`custom-checkbox-category-${index}`}
-                                                    name={razdel[0]}
-                                                    value={index}
-                                                    checked={selectedCategory[index]}
-                                                    onChange={() => {
-                                                        handleOnChangeCategory(index)
-                                                    }
-                                                    }
-                                                />
-                                                <label style={paddingNow} htmlFor={`custom-checkbox-category-${index}`}></label><span
-                                                className='spanList'>{razdel[0]}</span>
+                                    return (
+                                        <li key={index}>
+                                            <div className="toppings-list-item">
+                                                <div className="left-section">
+                                                    <input
+                                                        type="checkbox"
+                                                        id={`custom-checkbox-category-${index}`}
+                                                        name={razdel[0]}
+                                                        value={index}
+                                                        checked={selectedCategory[index]}
+                                                        onChange={() => {
+                                                            handleOnChangeCategory(index)
+                                                        }
+                                                        }
+                                                    />
+                                                    <label style={paddingNow}
+                                                           htmlFor={`custom-checkbox-category-${index}`}></label><span
+                                                    className='spanList'>{razdel[0]}</span>
+                                                </div>
                                             </div>
-                                        </div>
-                                        <ul>
-                                            {copy.map((stanok,i)=>{
-                                                let saveIndex = keyIndex++
-                                                return (
-                                                    <li key={saveIndex} className='individualLi'>
-                                                        <div className="toppings-list-item">
-                                                            <div className="left-section">
-                                                                <input
-                                                                    type="checkbox"
-                                                                    id={`custom-checkbox-${saveIndex}`}
-                                                                    name={stanok[0]}
-                                                                    value={`${saveIndex}`}
-                                                                    checked={selectedObjects[saveIndex]}
-                                                                    onChange={() => {
-                                                                        handleOnChange(saveIndex)}}
-                                                                />
-                                                                <label htmlFor={`custom-checkbox-${saveIndex}`}></label><span
-                                                                className='spanList'>{stanok[0]}</span>
+                                            <ul>
+                                                {copy.map((stanok, i) => {
+                                                    let saveIndex = keyIndex++
+                                                    return (
+                                                        <li key={saveIndex} className='individualLi'>
+                                                            <div className="toppings-list-item">
+                                                                <div className="left-section">
+                                                                    <input
+                                                                        type="checkbox"
+                                                                        id={`custom-checkbox-${saveIndex}`}
+                                                                        name={stanok[0]}
+                                                                        value={`${saveIndex}`}
+                                                                        checked={selectedObjects[saveIndex]}
+                                                                        onChange={() => {
+                                                                            handleOnChange(saveIndex)
+                                                                        }}
+                                                                    />
+                                                                    <label
+                                                                        htmlFor={`custom-checkbox-${saveIndex}`}></label><span
+                                                                    className='spanList'>{stanok[0]}</span>
+                                                                </div>
                                                             </div>
-                                                        </div>
-                                                    </li>
-                                                )
-                                            })}
-                                        </ul>
-                                    </li>
-                                );
+                                                        </li>
+                                                    )
+                                                })}
+                                            </ul>
+                                        </li>
+                                    );
 
-                            })}
-                        </ul>
-
+                                })}
+                            </ul>
+                        </div>
                     </div>
                 </div>
             </div>
