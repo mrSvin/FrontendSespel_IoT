06baef57109b31a402707898c2c2f5e793df3467
BookLove <krouvy@mail.ru>
2022-11-03 17:25:33 +0300

    Изменение состояний в форме.

--- static/js/components/skudAdmin.jsx
+++ static/js/components/skudAdmin.jsx
@@ -124,41 +124,58 @@ function SkudAdminForm({typeForm, setTypeForm, user, handleOnChange}) {
                        handleOnChange(e, 'tabel')
                    }}/>
             <label>Тип смены</label>
-            <select id="smena" name="smena">
-                <option value="1">Первая смена</option>
-                <option value="2">Вторая смена</option>
-                <option value="3">Третья смена</option>
-                <option value="А">Администрация</option>
+            <select id="smena" name="smena"
+                    onChange={(e) => {
+                        handleOnChange(e, 'tabel')
+                    }}>
+                <option value="Первая смена">Первая смена</option>
+                <option value="Вторая смена">Вторая смена</option>
+                <option value="Третья смена">Третья смена</option>
+                <option value="Администрация">Администрация</option>
             </select>
             <label>Длительность смены</label>
             <div className="smenaTimeSkud">
                 <div>
                     <label htmlFor="8hours">
-                        <input id='8hours' type="radio" name="longSmena" value="8"/>
+                        <input id='8hours' type="radio" name="long_smena" value="8 часов"
+                               onChange={(e) => {
+                                   handleOnChange(e, 'long_smena')
+                               }}/>
                         8 часов
                     </label>
                     <label htmlFor="7.2hours">
-                        <input id='7.2hours' type="radio" name="longSmena" value="7.2"/>
+                        <input id='7.2hours' type="radio" name="long_smena" value="7.2 часа"
+                               onChange={(e) => {
+                                   handleOnChange(e, 'long_smena')
+                               }}/>
                         7.2 часа
                     </label>
                 </div>
                 <div>
                     <label htmlFor="12hours">
-                        <input id='12hours' type="radio" name="longSmena" value="12"/>
+                        <input id='12hours' type="radio" name="long_smena" value="12 часов"
+                               onChange={(e) => {
+                                   handleOnChange(e, 'long_smena')
+                               }}/>
                         12 часов
                     </label>
                     <label htmlFor="24hours">
-                        <input id='24hours' type="radio" name="longSmena" value="24"/>
+                        <input id='24hours' type="radio" name="long_smena" value="24 часа"
+                               onChange={(e) => {
+                                   handleOnChange(e, 'long_smena')
+                               }}/>
                         24 часа
                     </label>
                 </div>
             </div>
 
             <label htmlFor="">Длительность обеда</label>
-            <select id="lunch" name="lunch">
-                <option value="1">30 минут</option>
-                <option value="2">60 минут</option>
-                <option value="3">90 минут</option>
+            <select id="lunch" name="lunch" onChange={(e) => {
+                handleOnChange(e, 'long_lunch')
+            }}>
+                <option value="30 минут">30 минут</option>
+                <option value="60 минут">60 минут</option>
+                <option value="90 минут">90 минут</option>
             </select>
             <button type="button">{typeForm == 'add' ? 'Добавить' : 'Изменить'}</button>
         </form>
