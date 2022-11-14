e5a00335bf25582d8adef73b2258b70d4ff82b08
BookLove <krouvy@mail.ru>
2022-11-14 17:15:54 +0300

    Отдельные компоненты обедов.

--- static/js/components/scud.jsx
+++ static/js/components/scud.jsx
@@ -134,17 +134,22 @@ function Scud() {
                 <DayCalendar newDate={newDate} date={date}/>
             </div>
 
-            {smenaState == '8' ? <СontainerEightHours heightHighchartContainer={heightHighchartContainer}/> : null}
+            {smenaState == '8' ? <LunchEightHours heightHighchartContainer={heightHighchartContainer}/> : null}
 
+            {smenaState == '7' ? <LunchSevenHours heightHighchartContainer={heightHighchartContainer}/> : null}
 
-            {smenaState == '8и' ? <СontainerItr heightHighchartContainer={heightHighchartContainer}/> : null}
+            {smenaState == '12' ? <LunchTwelveHours heightHighchartContainer={heightHighchartContainer}/> : null}
+
+            {smenaState == '24' ? <LunchTwentyFourHours heightHighchartContainer={heightHighchartContainer}/> : null}
+
+            {smenaState == '8и' ? <LunchItr heightHighchartContainer={heightHighchartContainer}/> : null}
 
 
         </div>
     );
 }
 
-function СontainerEightHours({heightHighchartContainer}) {
+function LunchEightHours({heightHighchartContainer}) {
     let height = {
         height: 52 * heightHighchartContainer
     };
@@ -191,7 +196,158 @@ function СontainerEightHours({heightHighchartContainer}) {
     )
 }
 
-function СontainerItr({heightHighchartContainer}) {
+function LunchSevenHours({heightHighchartContainer}) {
+    let height = {
+        height: 52 * heightHighchartContainer
+    };
+
+    let lunchSettings1 = {
+        height: getLunchHeight(heightHighchartContainer),
+        left: '34.9%',
+        width: '3%'
+    }
+
+    let lunchSettings2 = {
+        height: getLunchHeight(heightHighchartContainer),
+        left: '59%',
+        width: '3%'
+    }
+
+    let lunchSettings3 = {
+        height: getLunchHeight(heightHighchartContainer),
+        left: '81.5%',
+        width: '3%'
+    }
+
+    let pWidth = {
+        width: 'max-content',
+        left: '-45px'
+    }
+
+    return (
+        <div className='wrapperScud'>
+            <div className={'lunchTime'} style={lunchSettings1}>
+                <p style={pWidth}>Обед 11:30-12:30</p>
+                <div className='strokelunchTime'></div>
+            </div>
+            <div className={'lunchTime'} style={lunchSettings2}>
+                <p style={pWidth}>Обед 19:30-20:30</p>
+                <div className='strokelunchTime'></div>
+            </div>
+            <div className={'lunchTime'} style={lunchSettings3}>
+                <p style={pWidth}>Обед 03:00-4:00</p>
+                <div className='strokelunchTime'></div>
+            </div>
+            <div id="containerScud" style={height} className="scudHigcharts"></div>
+        </div>
+    )
+}
+
+function LunchTwelveHours({heightHighchartContainer}) {
+    let height = {
+        height: 52 * heightHighchartContainer
+    };
+
+    let lunchSettings1 = {
+        height: getLunchHeight(heightHighchartContainer),
+        left: '34.9%',
+        width: '1.5%'
+    }
+
+    let lunchSettings2 = {
+        height: getLunchHeight(heightHighchartContainer),
+        left: '48.6%',
+        width: '1.5%'
+    }
+
+    let lunchSettings3 = {
+        height: getLunchHeight(heightHighchartContainer),
+        left: '68.2%',
+        width: '1.5%'
+    }
+
+    let lunchSettings4 = {
+        height: getLunchHeight(heightHighchartContainer),
+        left: '83%',
+        width: '1.5%'
+    }
+
+    let pWidth = {
+        width: 'max-content',
+        left: '-50px'
+    }
+
+    return (
+        <div className='wrapperScud'>
+            <div className={'lunchTime'} style={lunchSettings1}>
+                <p style={pWidth}>Обед 11:30-12:00</p>
+                <div className='strokelunchTime'></div>
+            </div>
+            <div className={'lunchTime'} style={lunchSettings2}>
+                <p style={pWidth}>Обед 16:00-16:30</p>
+                <div className='strokelunchTime'></div>
+            </div>
+            <div className={'lunchTime'} style={lunchSettings3}>
+                <p style={pWidth}>Обед 23:00-23:30</p>
+                <div className='strokelunchTime'></div>
+            </div>
+            <div className={'lunchTime'} style={lunchSettings4}>
+                <p style={pWidth}>Обед 03:30-4:00</p>
+                <div className='strokelunchTime'></div>
+            </div>
+            <div id="containerScud" style={height} className="scudHigcharts"></div>
+        </div>
+    )
+}
+
+function LunchTwentyFourHours({heightHighchartContainer}) {
+    let height = {
+        height: 52 * heightHighchartContainer
+    };
+
+    let lunchSettings1 = {
+        height: getLunchHeight(heightHighchartContainer),
+        left: '38.1%',
+        width: '1.5%'
+    }
+
+    let lunchSettings2 = {
+        height: getLunchHeight(heightHighchartContainer),
+        left: '60.7%',
+        width: '1.5%'
+    }
+
+    let lunchSettings3 = {
+        height: getLunchHeight(heightHighchartContainer),
+        left: '81.5%',
+        left: '84.8%'
+    }
+
+    let pWidth = {
+        width: 'max-content',
+        left: '-50px'
+    }
+
+    return (
+        <div className='wrapperScud'>
+            <div className={'lunchTime'} style={lunchSettings1}>
+                <p style={pWidth}>Обед 12:30-13:00</p>
+                <div className='strokelunchTime'></div>
+            </div>
+            <div className={'lunchTime'} style={lunchSettings2}>
+                <p style={pWidth}>Обед 20:00-20:30</p>
+                <div className='strokelunchTime'></div>
+            </div>
+            <div className={'lunchTime'} style={lunchSettings3}>
+                <p style={pWidth}>Обед 04:00-04:30</p>
+                <div className='strokelunchTime'></div>
+            </div>
+            <div id="containerScud" style={height} className="scudHigcharts"></div>
+        </div>
+    )
+}
+
+function LunchItr({heightHighchartContainer}) {
     let height = {
         height: 52 * heightHighchartContainer
     };
