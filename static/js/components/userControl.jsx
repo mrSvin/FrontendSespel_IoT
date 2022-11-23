53b601a6c674342e91f28c8ffd82ff62fcfc20e3
BookLove <krouvy@mail.ru>
2022-11-23 13:03:55 +0300

    Подменю.

--- static/js/components/userControl.jsx
+++ static/js/components/userControl.jsx
@@ -218,10 +218,11 @@ function UsersControl() {
             <table className={`tableUsersControl`}>
                 <thead>
                 <tr>
-                    <th>Табельный</th>
-                    <th>Тип смены</th>
-                    <th>Длительность смены</th>
-                    <th>Длительность обеда</th>
+                    <th>ID</th>
+                    <th>Логин</th>
+                    <th>Права доступа</th>
+                    <th>Электронная почта</th>
+                    <th>Фото</th>
                     <th></th>
                     <th></th>
                 </tr>
@@ -235,6 +236,7 @@ function UsersControl() {
                                 <td>{userTable.type_smena}</td>
                                 <td>{userTable.long_smena}</td>
                                 <td>{userTable.long_lunch}</td>
+                                <td><div>Фото</div></td>
                                 <td>
                                     <div className='tdChange' onClick={() => {
                                         setUser(userTable)
@@ -281,79 +283,42 @@ function AdminFormUpdateAdd({
 
     return (
         <form className={classToTypeForm(typeForm)}>
-            <label htmlFor="">Табельный {typeForm == 'change' ? user.tabel : null}</label>
+            <label htmlFor="">Логин {typeForm == 'change' ? user.tabel : null}</label>
             <input className={typeForm == 'change' ? 'formHideUsersControl' : null}
                    value={user.tabel}
                    onChange={(e) => {
                        handleOnChange(e, 'tabel')
                    }}/>
-            <label>Тип смены</label>
+            <label htmlFor="">Пароль {typeForm == 'change' ? user.tabel : null}</label>
+            <input className={typeForm == 'change' ? 'formHideUsersControl' : null}
+                   value={user.tabel}
+                   onChange={(e) => {
+                       handleOnChange(e, 'tabel')
+                   }}/>
+            <label htmlFor="">Почта {typeForm == 'change' ? user.tabel : null}</label>
+            <input className={typeForm == 'change' ? 'formHideUsersControl' : null}
+                   value={user.tabel}
+                   onChange={(e) => {
+                       handleOnChange(e, 'tabel')
+                   }}/>
+            <label htmlFor="">Адрес фото {typeForm == 'change' ? user.tabel : null}</label>
+            <input className={typeForm == 'change' ? 'formHideUsersControl' : null}
+                   value={user.tabel}
+                   onChange={(e) => {
+                       handleOnChange(e, 'tabel')
+                   }}/>
+            <label>Права доступа</label>
             <div className='typeSmenaWrapper'>
-                <span className='yellowQuestion'>?
-                    <div className='infoBlockUsersControl'>1,2,3</div>
-                </span>
                 <select id="smena" name="smena" value={user.type_smena}
                         onChange={(e) => {
                             handleOnChange(e, 'type_smena')
                         }}>
-                    <option value="Первая смена">Первая смена</option>
-                    <option value="Вторая смена">Вторая смена</option>
-                    <option value="Третья смена">Третья смена</option>
-                    <option value="Администрация">Администрация</option>
+                    <option value="ROLE_USER">Пользователь</option>
+                    <option value="ROLE_ADMIN">Админ</option>
+                    <option value="ROLE_SERVICE">Сервисное обслуживание</option>
                 </select>
             </div>
-            <label>Длительность смены</label>
-            <div className="smenaTimeScud">
-                <div>
-                    <label htmlFor="8hours">
-                        <input id='8hours' type="radio" name="long_smena" value='8 ч.'
-                               checked={user.long_smenaBool['8 ч.']}
-                               onChange={(e) => {
-                                   handleOnChange(e, 'long_smena')
-                                   handleOnChangeBool(e)
-                               }}/>
-                        8 часов
-                    </label>
-                    <label htmlFor="7.2hours">
-                        <input id='7.2hours' type="radio" name="long_smena" value='7.2 ч.'
-                               checked={user.long_smenaBool['7.2 ч.']}
-                               onChange={(e) => {
-                                   handleOnChange(e, 'long_smena')
-                                   handleOnChangeBool(e)
-                               }}/>
-                        7.2 часа
-                    </label>
-                </div>
-                <div>
-                    <label htmlFor="12hours">
-                        <input id='12hours' type="radio" name="long_smena" value='12 ч.'
-                               checked={user.long_smenaBool['12 ч.']}
-                               onChange={(e) => {
-                                   handleOnChange(e, 'long_smena')
-                                   handleOnChangeBool(e)
-                               }}/>
-                        12 часов
-                    </label>
-                    <label htmlFor="24hours">
-                        <input id='24hours' type="radio" name="long_smena" value='24 ч.'
-                               checked={user.long_smenaBool['24 ч.']}
-                               onChange={(e) => {
-                                   handleOnChange(e, 'long_smena')
-                                   handleOnChangeBool(e)
-                               }}/>
-                        24 часа
-                    </label>
-                </div>
-            </div>
 
-            <label htmlFor="">Длительность обеда</label>
-            <select id="lunch" name="lunch" value={user.long_lunch} onChange={(e) => {
-                handleOnChange(e, 'long_lunch')
-            }}>
-                <option value="30 минут">30 минут</option>
-                <option value="60 минут">60 минут</option>
-                <option value="90 минут">90 минут</option>
-            </select>
             <button type="button"
                     onClick={() => {
                         if (user.type_smena == '' || user.tabel == '' || user.long_smena == '' || user.long_lunch == '') {
