60c9cc0f3992956327f0aa9c99fef1789cecd077
BookLove <krouvy@mail.ru>
2023-02-15 17:17:42 +0300

    Читабельность vs Ctrl+L

--- static/js/components/menubar.jsx
+++ static/js/components/menubar.jsx
@@ -22,49 +22,14 @@ function IframeLink({source}) {
 
 function Menubar() {
     const menuItems = [
-        {
-            name: 'Станки',
-            link: '/stanki/ОТК',
-            iconClass: 'stanki',
-            description: 'Суточные и месячные отчеты работы оборудования'
-        },
-        {
-            name: 'Энергоресурсы',
-            link: '/energyWater',
-            iconClass: 'energy',
-            description: 'Показатели расхода газа, воды, электроэнергии'
-        },
-        {name: 'Beacon', link: '/beacon', iconClass: 'beacon', description: 'Отслеживание объектов'},
-        {
-            name: 'Контроль персонала',
-            link: '/scud/2ploshadka',
-            iconClass: 'scud',
-            description: 'Отслеживание работы сотрудников в зонах предприятия'
-        },
-        {
-            name: 'Owencloud',
-            link: '/owencloud',
-            iconClass: 'owencloud',
-            description: 'Облачная платформа мониторинга оборудования'
-        },
-        {
-            name: 'Умный дом',
-            link: '/intra',
-            iconClass: 'intra',
-            description: 'SCADA система Intrahouse для диспетчеризации'
-        },
-        {
-            name: 'Wialon',
-            link: '/wialon',
-            iconClass: 'wialon',
-            description: 'Облачная платформа мониторинга производимых ППЦ'
-        },
-        {
-            name: 'Teamcenter',
-            link: '/teamcenter',
-            iconClass: 'teamcenter',
-            description: 'Платформа для работы с конструкторской документацией'
-        },
+        { name: 'Станки', link: '/stanki/ОТК', iconClass: 'stanki', description: 'Суточные и месячные отчеты работы оборудования' },
+        { name: 'Энергоресурсы', link: '/energyWater', iconClass: 'energy', description: 'Показатели расхода газа, воды, электроэнергии' },
+        { name: 'Beacon', link: '/beacon', iconClass: 'beacon', description: 'Отслеживание объектов' },
+        { name: 'Контроль персонала', link: '/scud/2ploshadka', iconClass: 'scud', description: 'Отслеживание работы сотрудников в зонах предприятия' },
+        { name: 'Owencloud', link: '/owencloud', iconClass: 'owencloud', description: 'Облачная платформа мониторинга оборудования' },
+        { name: 'Умный дом', link: '/intra', iconClass: 'intra', description: 'SCADA система Intrahouse для диспетчеризации' },
+        { name: 'Wialon', link: '/wialon', iconClass: 'wialon', description: 'Облачная платформа мониторинга производимых ППЦ' },
+        { name: 'Teamcenter', link: '/teamcenter', iconClass: 'teamcenter', description: 'Платформа для работы с конструкторской документацией' },
     ];
 
     return (
