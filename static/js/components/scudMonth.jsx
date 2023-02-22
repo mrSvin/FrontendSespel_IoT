09738c0d73b1a3a330ce77678ba3f2c21f7a2f85
BookLove <krouvy@mail.ru>
2023-02-22 13:54:48 +0300

    Защита от manyRequest.

--- static/js/components/scudMonth.jsx
+++ static/js/components/scudMonth.jsx
@@ -43,66 +43,72 @@ function ScudMonth({scudMonthMemory, setScudMonthMemory}) {
     function fetchRequestScudMonthThen(promise) {
         promise.then(data => {
             let usersData = data[0]
-            let date = data[1]
 
-            if (!Object.keys(usersData).includes('error')) {
-                usersData = createUserDataStructure(usersData)
+            if (usersData == 'many request') {
+                setLoading(false)
+                saveMemoryMonth()
+            } else{
+                let date = data[1]
 
-                let userNames = Object.keys(usersData)
-                usersData = applyMonthFilters(usersData, userNames, date)
+                if (!Object.keys(usersData).includes('error')) {
+                    usersData = createUserDataStructure(usersData)
 
-                let allData = {
-                    smena_7: [],
-                    smena_8i: [],
-                    smena_8: [],
-                    smena_11: [],
-                    smena_24: [],
-                    hiddens: []
-                }
+                    let userNames = Object.keys(usersData)
+                    usersData = applyMonthFilters(usersData, userNames, date)
 
-                Object.keys(usersData).forEach(name => {
-
-                    switch (usersData[name].smenaInfo) {
-                        case '7':
-                            allData.smena_7.push(usersData[name])
-                            break
-                        case '8и':
-                            allData.smena_8i.push(usersData[name])
-                            break
-                        case '8':
-                            allData.smena_8.push(usersData[name])
-                            break
-                        case '11':
-                            allData.smena_11.push(usersData[name])
-                            break
-                        case '24':
-                            allData.smena_24.push(usersData[name])
-                            break
-                        default:
-                            allData.hiddens.push(usersData[name])
+                    let allData = {
+                        smena_7: [],
+                        smena_8i: [],
+                        smena_8: [],
+                        smena_11: [],
+                        smena_24: [],
+                        hiddens: []
                     }
-                })
 
-                if (scudMonthMemory == null) {
-                    setScudMonthMemory({[dateMonth]: {lastTime: dayTimeNow(), data: allData},})
-                } else if (scudMonthMemory[dateMonth] == undefined) {
-                    setScudMonthMemory(prevState => ({
-                        ...prevState,
-                        [dateMonth]: {lastTime: dayTimeNow(), data: allData}
-                    }));
-                } else {
-                    let lastTime = new Date(dayTimeNow()).getTime()
-                    let thisMonthLastTime = new Date(scudMonthMemory[dateMonth].lastTime).getTime()
-                    // Если данные записанные больше часа назад
-                    if (lastTime - thisMonthLastTime > 10800000) {
+                    Object.keys(usersData).forEach(name => {
+
+                        switch (usersData[name].smenaInfo) {
+                            case '7':
+                                allData.smena_7.push(usersData[name])
+                                break
+                            case '8и':
+                                allData.smena_8i.push(usersData[name])
+                                break
+                            case '8':
+                                allData.smena_8.push(usersData[name])
+                                break
+                            case '11':
+                                allData.smena_11.push(usersData[name])
+                                break
+                            case '24':
+                                allData.smena_24.push(usersData[name])
+                                break
+                            default:
+                                allData.hiddens.push(usersData[name])
+                        }
+                    })
+
+                    if (scudMonthMemory == null) {
+                        setScudMonthMemory({[dateMonth]: {lastTime: dayTimeNow(), data: allData},})
+                    } else if (scudMonthMemory[dateMonth] == undefined) {
                         setScudMonthMemory(prevState => ({
                             ...prevState,
                             [dateMonth]: {lastTime: dayTimeNow(), data: allData}
                         }));
+                    } else {
+                        let lastTime = new Date(dayTimeNow()).getTime()
+                        let thisMonthLastTime = new Date(scudMonthMemory[dateMonth].lastTime).getTime()
+                        // Если данные записанные больше часа назад
+                        if (lastTime - thisMonthLastTime > 10800000) {
+                            setScudMonthMemory(prevState => ({
+                                ...prevState,
+                                [dateMonth]: {lastTime: dayTimeNow(), data: allData}
+                            }));
+                        }
                     }
-                }
 
-                switchTableState(allData)
+                    switchTableState(allData)
+                }
             }
         })
     }
